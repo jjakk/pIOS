66528737beec15c75053fabcc80d54483c3b1378
Jak <jaksawesome10@github.com>
2018-09-18 15:38:59 -0400

    stuff

--- newPIOS/js/engine.js
+++ newPIOS/js/engine.js
@@ -1,17 +1,27 @@
 
-
 function keyPressed(event) {
+	
+	lineShift = document.getElementById("selector").style.top;
+	
 	//Up Key
 	if (event.keyCode == 38 || event.keyCode == 87) {
+		
+		lineShift -= 30;
 		
 	}
 	//Down Key
 	if (event.keyCode == 40 || event.keyCode == 83) {
-			
+		
+		console.log("dfghjk")
+		
+		lineShift += 33;
+		
 	}
 	if (event.keyCode == 13) {
 		
 		
 	}
+	
+	document.getElementById("selector").style.top = lineShift;
 	
 }
