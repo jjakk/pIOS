2d8bea39f6e8c48a45ad5d215c040da15e7dad00
Jak <jaksawesome10@github.com>
2018-09-17 22:33:06 -0400

    good stuff

--- newPIOS/js/engine.js
+++ newPIOS/js/engine.js
@@ -1,20 +1,16 @@
 
 
 function keyPressed(event) {
-	startBackgroundMusic();
-	keyFirstPressed = true;
-	if(buttonsEnabled == true){
-		clearViewWindow();
-		if (event.keyCode == 38 || event.keyCode == 87) {
+	//Up Key
+	if (event.keyCode == 38 || event.keyCode == 87) {
+		
+	}
+	//Down Key
+	if (event.keyCode == 40 || event.keyCode == 83) {
 			
-		}
-		if (event.keyCode == 40 || event.keyCode == 83) {
-			
-		}
-		if (event.keyCode == 13) {
-			
-			
-		}
+	}
+	if (event.keyCode == 13) {
+		
 		
 	}
 	
